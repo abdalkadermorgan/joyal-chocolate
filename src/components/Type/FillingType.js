e32b7536a2cfa56f775f3d2be4654aa37a4a9050
Abd Alkader Morjan <77734227+abdalkadermorgan@users.noreply.github.com>
2022-06-20 17:08:07 +0300

    sub box

--- src/components/Type/FillingType.js
+++ src/components/Type/FillingType.js
@@ -1,51 +1,66 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 import { useState } from "react";
-import { Form, Tab, Tabs } from "react-bootstrap";
+import { Tab, Tabs } from "react-bootstrap";
 import { images } from "../../assets/images";
 import data from "../data.json";
-import Marge from "./Marge";
 
 const FillingType = () => {
   const [key, setKey] = useState('');
 
   const chocolate_type = data.chocolate_type;
   const filling_type = chocolate_type.map((type) => type.filling_type);
-  const merge = filling_type.map((merge) => merge);
+  // const merge = filling_type.map((merge) => merge);
 
-  const selectTab = (type, index) => {
-      console.log(type);
-      console.log(index);
-      setKey(`${type}-${index}`);
-};
 
-//   console.log("c =>", chocolate_type);
-//   console.log("f =>", merge);
+  //   console.log("c =>", chocolate_type);
+  //   console.log("f =>", merge);
   console.log("kkk =>", key);
-
   return chocolate_type.map((type, i) => (
+
     <div className="select-filling" key={`filing-${i}`}>
       <div className="chocolate-content">
         <div className="chocolate-img">
           <img src={images.ovalChocolate} alt="" />
         </div>
-        <h4>{type.name} {i}</h4>
+        <h4>{type.name}</h4>
       </div>
-      <Tabs
-        id={type.id}
-        activeKey={key}
-        // onSelect={(k) => setKey(k)}
-        onSelect={(k) => setKey(k)}
-        className="mb-3"
+      <div
+        className='tab-main'
       >
-        {type.filling_type.map((filling, index) => (
-          <Tab
-            eventKey={index}
-            key={`filling-type-${index}`}
-            title={<div>{filling.name} {index}</div>}
-          >
-            <div>{filling.marge.map((marge) => marge.name)}</div>
-          </Tab>
-        ))}
-      </Tabs>
+        <Swiper
+          navigation={true} 
+          modules={[Navigation]} 
+          className="mySwiper"
+          spaceBetween={10}
+          slidesPerView={3}
+        >
+          {type.filling_type.map((filling, index) => (
+            <SwiperSlide>
+              <div className='filling-type' key={`filling-type-${index}`}>
+                <div className="tab-header">
+                  <div className="tab-header_img">
+                    <img src={images.biscuitFilling} alt="" />
+                  </div>
+                  <div className="tab-header_title">
+                    <h4>
+                      {filling.name}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* <div className="marge-with">
+              <span>Marge With: </span>
+              {filling.marge.map((marge) => (
+                <p>{marge.name}</p>
+              ))}
+            </div> */}
+      </div>
     </div>
   ));
 };
